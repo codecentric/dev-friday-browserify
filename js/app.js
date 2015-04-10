55279d8e7b2520cdbaa3604ed249e5bc17069bb9
Bastian Krol <bastian.krol@web.de>
2015-04-10 10:50:49 +0200

    extract util

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 var $ = require('jquery');
 var Handlebars = require('handlebars');
 var Router = require('director/build/director').Router;
+var util = require('./util');
 
 $(function() {
 
@@ -12,35 +13,6 @@ $(function() {
 
 	var ENTER_KEY = 13;
 	var ESCAPE_KEY = 27;
-
-	var util = {
-		uuid: function () {
-			/*jshint bitwise:false */
-			var i, random;
-			var uuid = '';
-
-			for (i = 0; i < 32; i++) {
-				random = Math.random() * 16 | 0;
-				if (i === 8 || i === 12 || i === 16 || i === 20) {
-					uuid += '-';
-				}
-				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-			}
-
-			return uuid;
-		},
-		pluralize: function (count, word) {
-			return count === 1 ? word : word + 's';
-		},
-		store: function (namespace, data) {
-			if (arguments.length > 1) {
-				return localStorage.setItem(namespace, JSON.stringify(data));
-			} else {
-				var store = localStorage.getItem(namespace);
-				return (store && JSON.parse(store)) || [];
-			}
-		}
-	};
 
 	var App = {
 		init: function () {
